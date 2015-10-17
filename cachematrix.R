48fa14dbfa32c8e890e09c301d5ce065b90fb47f
Christian Albers <quac.albers@gmail.com>
2015-10-18 01:53:02 +0200

    Modified comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,50 +1,45 @@
 ## This is my attempt at programming assignment 2 of the course 
 ## R Programming
 ##
-## There are two functions defined here.
-## The structure in general is the same as the one given by the example
-## functions in the programming assignment.
 
 ## makeCacheMatrix takes a square matrix and outputs a list
 ## of names that refer to functions defined inside. These functions are
-## set, get, setinv, getinv, which do:
-## set: Sets the stored matrix
-## get: Outputs the stored matrix
-## setinv: Sets the inverse of the stored matrix
-## getinv: Outputs the inverse
+## set, get, setinv, getinv.
+## Usage:
+## Convert a matrix to cached matrix:
+## mat <- makeCachedMatrix(mat)
+##
+## mat$get()    - Output stored matrix
+## mat$set()    - Store a new matrix in mat (erases previous matrix)
+## mat$getinv() - Output cached inverse. Is NULL if cacheSolve has not
+##                been called before
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-    
-    # inital test for matrix dimensions. Only square matrices
-    # (num_rows == num_cols) can be inverted!
+
+    # inital test for matrix dimensions (is square matrix?)
     if(dim(x)[1] != dim(x)[2]) {
         message("The matrix is not square and can not be inverted!")
-        return(x) # return the matrix if test failed.
+        return(x) # return the matrix if it is non-square (and non-invertible)
     }
     
-    # From here the code is in principle the same as in vector mean example
     invm <- NULL # default value of the inverse; NULL
     
-    # The function set can be used to change the stored matrix after
-    # the first call of makeCachedMatrix
-    set <- function(y) {
+    set <- function(y) { # Store new matrix
         x <<- y
-        invm <<- NULL # in case of changing matrix, set stored inv to default
+        invm <<- NULL # in case of changing matrix, set inverse to default
     }
-    # The function get is used to access the stored matrix
-    get <- function() x
-    # setinv sets the stored inverse
-    setinv <- function(inv) invm <<- inv
-    # getinv outputs the stored inverse
-    getinv <- function() invm
+    get <- function() x # simply return stored matrix x
+    setinv <- function(inv) invm <<- inv # store new inverse inv as invm
+    getinv <- function() invm # simply return stored inverse invm
+    
+    # return list of function as names
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-## cacheSolve is tailored to work on the list
-## output by makeCacheMatrix. As argument it takes the list, accesses
-## the stored inverse via getinv, and in case the inverse is NULL
-## (default value), cacheSolve computes the inverse and stores it
-## inside makeCacheMatrix by calling setinv
+## cacheSolve takes as argument the list output by makeCacheMatrix.
+## It outputs the inverse. On first call it computes the inverse using
+## solve, in all subsequent calls it returns the cached inverse.
 
 cacheSolve <- function(x, ...) {
     inv <- x$getinv() # get the stored inverse
